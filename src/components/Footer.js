cb9f80512dc1d3510211efc10e38bd1555ea638b
Damián Andrés Cabrio <soy@damiancabrio.com.ar>
2022-08-15 19:12:26 -0300

    Basic Footer structure

---
+++ src/components/Footer.js
@@ -0,0 +1,28 @@
+const Footer = () => {
+  return (
+    <footer>
+      <nav>
+        <ul>
+          <li>
+            <a href="https://www.facebook.com/" rel="noopener noreferrer">
+              Facebook
+            </a>
+          </li>
+          <li>
+            <a href="https://www.twitter.com/" rel="noopener noreferrer">
+              Twitter
+            </a>
+          </li>
+          <li>
+            <a href="https://www.instagram.com/" rel="noopener noreferrer">
+              Instagram
+            </a>
+          </li>
+        </ul>
+      </nav>
+      <p>Copyright: Damián Cabrio</p>
+    </footer>
+  );
+};
+
+export default Footer;
