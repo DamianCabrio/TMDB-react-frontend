6075e92e424b5d50103492a182f29b92ed44da54
Damián Andrés Cabrio <soy@damiancabrio.com.ar>
2022-08-15 19:25:23 -0300

    Added bootstrap to project

--- src/App.js
+++ src/App.js
@@ -2,15 +2,21 @@ import { Routes, Route } from 'react-router-dom';
 
 import Login from './components/Login';
 import List from './components/List';
+import Header from './components/Header';
+import Footer from './components/Footer';
+
+import './css/bootstrap.min.css';
 
 function App() {
   return (
-    <>
+    <div className='container'>
+      <Header />
       <Routes>
         <Route path="/" element={<Login />} />
         <Route path="/list" element={<List />} />
       </Routes>
-    </>
+      <Footer />
+    </div>
   );
 }
 
